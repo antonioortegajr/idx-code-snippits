26e13fca69524713acc43512cbf49262edbf02d5
antonioortegajr <antonioortegajr@gmail.com>
2017-09-01 14:29:06 -0700

    Add script
    add code for JS only approach

---
+++ custom-404-details/pure-js-approach/script.js
@@ -0,0 +1,10 @@
+
+var currentUrl = window.location.href;
+if(currentUrl.indexOf('/idx/')) {
+
+var fourOhfour = document.getElementsByTagName('small')[0];
+if(fourOhfour = '<small>the request url does not exist</small>') {
+document.getElementsByTagName('h1')[0].innerHTML='nope';
+}
+
+}
