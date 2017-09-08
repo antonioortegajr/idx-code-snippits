68713c323a779b06842fc7356663041a175c547c
antonioortegajr <antonioortegajr@gmail.com>
2017-09-08 16:57:18 -0700

    Update script.js
    added cat API

--- custom-404-details/pure-js-approach/script.js
+++ custom-404-details/pure-js-approach/script.js
@@ -1,6 +1,6 @@
 <script>
 var currentUrl = window.location.href;
 if(currentUrl.indexOf('/idx/') && document.querySelector('#IDX-detailsMissingPropertyAlert') !== null ) {
-document.querySelector('#IDX-detailsMissingPropertyAlert').innerHTML='nope';
+document.querySelector('#IDX-detailsMissingPropertyAlert').innerHTML='Property not found but here are some kittens<br><a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>';
 };
 </script>
